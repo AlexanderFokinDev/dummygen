a8f78d181acb4d78032b2e0d669277448a58cdad
Alexander Fokin <alexander.fokin@improvado.io>
2023-01-21 16:02:45 -0300

    CSV file can be downloaded

--- src/main/kotlin/uy/amn/dummygen/presentation/controllers/DummyTableController.kt
+++ src/main/kotlin/uy/amn/dummygen/presentation/controllers/DummyTableController.kt
@@ -3,9 +3,16 @@ package uy.amn.dummygen.presentation.controllers
 import com.opencsv.bean.StatefulBeanToCsvBuilder
 import com.opencsv.exceptions.CsvDataTypeMismatchException
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.ContentDisposition
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 import uy.amn.dummygen.domain.DummyTable
+import java.io.File
+import java.io.FileInputStream
 import java.io.FileWriter
 import java.io.IOException
 
@@ -17,13 +24,22 @@ class DummyTableController {
         return ModelAndView("form")
     }
 
-    @PostMapping("/dummy-table")
-    fun saveDummyTable(@RequestParam rows: Int, @RequestParam columns: Int, @RequestParam email: String): String {
+    @PostMapping("/submit-dummy-table")
+    fun submit(
+        @RequestParam rows: Int,
+        @RequestParam columns: Int,
+        @RequestParam email: String
+    ): ResponseEntity<InputStreamResource> {
+
         val dummyTable = DummyTable(rows, columns, email)
+
+        val file = File("dummy_table_${rows}_rows.csv")
+        file.delete()
+
         try {
-            val writer = FileWriter("dummy_table.csv")
-            val beanToCsv = StatefulBeanToCsvBuilder<DummyTable>(writer).build()
-            beanToCsv.write(dummyTable)
+            val writer = FileWriter(file)
+            val csvWriter = StatefulBeanToCsvBuilder<DummyTable>(writer).build()
+            csvWriter.write(dummyTable)
             writer.close()
         } catch (e: CsvDataTypeMismatchException) {
             e.printStackTrace()
@@ -32,6 +48,15 @@ class DummyTableController {
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        return "Dummy Table saved successfully"
+
+        val inputStream = FileInputStream(file)
+        val inputStreamResource = InputStreamResource(inputStream)
+
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_OCTET_STREAM
+        headers.contentLength = file.length()
+        headers.contentDisposition = ContentDisposition.builder("attachment").filename(file.name).build()
+
+        return ResponseEntity.ok().headers(headers).body(inputStreamResource)
     }
 }
